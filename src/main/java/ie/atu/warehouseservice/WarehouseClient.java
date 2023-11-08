2b9aa75c0194d39914dd9f8985e4603784b9eb8f
Igor <G00390368@atu.ie>
2023-11-08 10:20:30 +0000

    added the warehouse feature

--- src/main/java/ie/atu/warehouseservice/WarehouseClient.java
+++ src/main/java/ie/atu/warehouseservice/WarehouseClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name ="Warehouse", url = "http/localhost:8081")
+@FeignClient(name ="Warehouse-Service", url = "http/localhost:8081")
 public interface WarehouseClient {
     @GetMapping ("/product")
     String  postData(@RequestBody ProductDetails productdetails);
--- src/main/java/ie/atu/warehouseservice/WarehouseController.java
+++ src/main/java/ie/atu/warehouseservice/WarehouseController.java
@@ -1,8 +1,13 @@
 package ie.atu.warehouseservice;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 public class WarehouseController {
@@ -11,4 +16,13 @@ public class WarehouseController {
         String confrimMessage  = String.format("Recieved details");
         return confrimMessage;
     }
+    @GetMapping("/products")
+    public String getWarehouseById()throws Exception, InterruptedException{
+        List<CompletableFuture<ProductDetails>> future = new ArrayList<>();
+        CompletableFuture.supplyAsync(()->productService.addData());
+        futures.add(future);
+    }
+    CompletableFuture<Void>allOf = CompletableFuture.allOf(futures.toArray(new CompletableFuture([0]));
+        allOf.get();
+        return futures;
 }
